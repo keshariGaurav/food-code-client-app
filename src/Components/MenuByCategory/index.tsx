35778cbdbdb88831fb7014f7f9ca9df9d3f5f1eb
Ayushi <khandeliaayushi08@gmail.com>
2024-06-03 07:35:17 +0530

    added lazy loading in menuitem

--- src/Components/MenuByCategory/index.tsx
+++ src/Components/MenuByCategory/index.tsx
@@ -1,5 +1,8 @@
-import { MenuItemByCategory } from '../../types';
-import MenuItemWrapper from '../MenuItemWrapper';
+import React, { Suspense, lazy, useState, useEffect } from 'react';
+import { MenuItemByCategory, MenuItems } from '../../types';
+import { useInView } from 'react-intersection-observer';
+
+const MenuItemWrapper = lazy(() => import('../MenuItemWrapper'));
 
 interface MenuByCategoryProps {
   menuItem: MenuItemByCategory;
@@ -14,9 +17,32 @@ const MenuByCategory = ({ menuItem }: MenuByCategoryProps) => {
         <p className="p-1 text-lg uppercase text-gray-500">{menuItem.category.name}</p>
       </div>
       {menuItem.menus.map((menu) => (
-        <MenuItemWrapper key={menu.id} menu={menu} />
+        <LazyMenuItemWrapper key={menu.id} menu={menu} />
       ))}
     </>
+  );
+};
+
+interface LazyMenuItemWrapperProps {
+  menu: MenuItems;
+}
+
+const LazyMenuItemWrapper = ({ menu }: LazyMenuItemWrapperProps) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
+  return (
+    <div ref={ref} style={{ minHeight: '100px' }}>
+      {inView ? (
+        <Suspense fallback={<div>Loading...</div>}>
+          <MenuItemWrapper menu={menu} />
+        </Suspense>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
   );
 };
 
