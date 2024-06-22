04f80d94e3a8d90e794c35cf3ebece5904734a12
gaurav keshari <kesharigaurav97@gmail.com>
2024-06-22 12:12:28 +0530

    feat(search-handler): Create Search handler functionality.
    1)Create search handler functionality on search bar of all menu items which searches on based of categories and menu name and menu description debouncing effect have been implemented.
    
    2)Create a copy data and mark visible to true or false based on matches to show into result.

--- src/Components/AllMenusWrapper/index.tsx
+++ src/Components/AllMenusWrapper/index.tsx
@@ -10,16 +10,55 @@ import MenuItemDescription from '@/Components/MenuItemDescription';
 
 const AllMenusWrapper = () => {
   const [menuItems, setMenuItems] = useState<MenuItemByCategory[]>([]);
+  const [modifiedMenuItems, setMofifiedMenuItems] = useState<MenuItemByCategory[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const popupMenuState = useSelector((state: RootState) => state.menuPopup);
   const isVisible = popupMenuState.visible;
+
+  function changeMenuItemsVisibility(
+    searchTerm: string,
+    data: MenuItemByCategory[],
+  ): void {
+    const copyData = [...data];
+    for (const category of copyData) {
+      category.visible = false;
+      for (const item of category.menus) {
+        item.visible = false;
+      }
+    }
+
+    for (const category of copyData) {
+      if (category.category.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+        category.visible = true;
+        for (const item of category.menus) {
+          item.visible = true;
+        }
+      }
+
+      for (const item of category.menus) {
+        if (
+          item.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          item.description.toLowerCase().includes(searchTerm.toLowerCase())
+        ) {
+          item.visible = true;
+          category.visible = true;
+        }
+      }
+    }
+    setMofifiedMenuItems(copyData);
+  }
+
+  const handleSearch = (text: string) => {
+    changeMenuItemsVisibility(text, menuItems);
+  };
 
   useEffect(() => {
     const fetchMenuItems = async () => {
       try {
         const response = await axios.get('http://localhost:3100/api/v1/menus/category');
         setMenuItems(response.data.data);
+        setMofifiedMenuItems(response.data.data);
       } catch (err: any) {
         setError(err.message);
       } finally {
@@ -30,6 +69,10 @@ const AllMenusWrapper = () => {
     fetchMenuItems();
   }, []);
 
+  useEffect(() => {
+    handleSearch('');
+  }, [menuItems]);
+
   if (loading) return <div key="loading">Loading...</div>;
   if (error) return <div>Error: {error}</div>;
 
@@ -38,15 +81,15 @@ const AllMenusWrapper = () => {
       <div>
         <div className="mb-20">
           <div className="fixed left-0 right-0 top-0 z-20 bg-white pt-2">
-            <SearchBar />
+            <SearchBar handler={handleSearch} />
           </div>
         </div>
-        {menuItems.map((menuItem: MenuItemByCategory, index) => (
+        {modifiedMenuItems.map((menuItem: MenuItemByCategory, index) => (
           <div
             key={index}
             className={`${isVisible ? 'z-index-1 overscroll-none' : 'blur-none'}`}
           >
-            <MenuByCategory menuItem={menuItem} key={menuItem.id} />
+            {menuItem.visible && <MenuByCategory menuItem={menuItem} key={menuItem.id} />}
           </div>
         ))}
       </div>
---
+++ src/utils/debounce.ts
@@ -0,0 +1,19 @@
+export function debounce<T extends (...args: any[]) => void>(
+  func: T,
+  wait: number,
+): (...args: Parameters<T>) => void {
+  let timeout: NodeJS.Timeout | null;
+
+  return function executedFunction(...args: Parameters<T>): void {
+    const later = () => {
+      timeout = null;
+      func(...args);
+    };
+
+    if (timeout !== null) {
+      clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(later, wait);
+  };
+}
